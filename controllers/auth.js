cd566843e9a8ff23c95041200d549181dd2e1d49
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-11-09 15:13:31 -0500

    Crear usuario en base de datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,39 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
     const { email, name, password } = req.body;
-    console.log(email, name, password);
+    try {
+        //Verificar el email
+        const usuario = await Usuario.findOne({email});
+        if(usuario) {
+            return res.status(400).json({
+                ok: false, 
+                msg: 'El usuario ya existe con ese email'
+            });
+        }
+
+        //Crear usuario con el modelo
+        const dbUser = new Usuario(req.body);
+
+        //Crear usuario de DB
+        await dbUser.save();
+
+        //Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id, 
+            name
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.json({
+            ok: false, 
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+    
     return res.json({
         ok: true, 
         msg: 'Crear usuario /new'
@@ -11,7 +42,7 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res = response) => {
     const { email, password } = req.body;
-    console.log(email, password);
+    
     return res.json({
         ok: true, 
         msg: 'Login de usuario /'
